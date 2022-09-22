3a26b27e18c328b1f82e2406efb18b0fe071b7ac
nguyengiathang190402 <94787151+nguyengiathang190402@users.noreply.github.com>
2022-09-22 09:01:44 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    introTitle: 'Babe à!',
+    introDesc: `Em có muốn sau này con em vừa có má lúm
+    vừa có răng khểnh không`,
+    btnIntro: 'Có ạ ❤️',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là Yêu anh <33',
+    btnNo: 'Không, Anh Yêu à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Vì anh Thắng đẹp trai vãi cả chưởng Moazzzzz <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối nay anh qua đón nhé công chúa. Đừng quên ib cho anh',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/G.thwzang02' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
